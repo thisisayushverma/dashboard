8e18cc1b15067dc8e1c7209e6f1acf3cfa3a163b
Ayush Verma <devilofilluminati@gmail.com>
2024-11-30 16:54:51 +0530

    Horizontal tracker complete

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,13 +2,15 @@ import Navbar from "./navbar";
 import NavbarItem from "./navbarItem";
 import People from "./People.svg"
 import SideNavbar from "./sidenavbar";
+import TrackerHori from "./trackerHori"
 
 const Dashboard = () => {
     return (
       <div>
         {/* <NavbarItem  icon={People} name="Home"  href="/"/> */}
         {/* <Navbar/> */}
-        <SideNavbar/>
+        {/* <SideNavbar/> */}
+        <TrackerHori imageSrc="https://placehold.co/49x32" name="Food Safety" value={75}/>
         <h1>Welcome to the Dashboard</h1>
         <p>This is the /dashboard route.</p>
       </div>
